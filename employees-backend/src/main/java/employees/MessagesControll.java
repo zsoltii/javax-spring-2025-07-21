8f0b94cc3691b6af8f3803f8f78a29be11e2393a
Fatér Zsolt <zsolt.fater@idomsoft.hu>
2025-07-22 13:49:17 +0200

    sse complitableFuture

--- employees-backend/src/main/java/employees/MessagesControll.java
+++ employees-backend/src/main/java/employees/MessagesControll.java
@@ -3,11 +3,10 @@ package employees;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.io.IOException;
 
 @Controller
 @Slf4j
@@ -21,6 +20,14 @@ public class MessagesControll {
     public SseEmitter messages() {
         SseEmitter emitter = new SseEmitter();
         messagesService.counter(emitter);
+        return emitter;
+    }
+
+    @GetMapping("api/messages-cf")
+    @SneakyThrows
+    public SseEmitter messagesCompletableFuture() {
+        SseEmitter emitter = new SseEmitter();
+        messagesService.counterCompletableFuture(emitter);
         return emitter;
     }
 }
--- employees-backend/src/main/java/employees/MessagesService.java
+++ employees-backend/src/main/java/employees/MessagesService.java
@@ -5,15 +5,31 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
 @Service
-@Async
 public class MessagesService {
 
+    @Async
     public void counter(SseEmitter emitter) throws InterruptedException, IOException {
         for (int i = 1; i <= 10; i++) {
             Thread.sleep(1000); // Simulate delay
             emitter.send("Message " + i);
         }
+        emitter.complete();
+    }
+
+    public CompletableFuture<Void> counterCompletableFuture(SseEmitter emitter) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                for (int i = 1; i <= 10; i++) {
+                    Thread.sleep(1000); // Simulate delay
+                    emitter.send("CF Message " + i);
+                }
+            } catch (Exception e) {
+                emitter.completeWithError(e);
+            }
+            emitter.complete();
+        });
     }
 }
