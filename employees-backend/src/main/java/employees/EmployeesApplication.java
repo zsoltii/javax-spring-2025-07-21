ee216becd084a5ac2045d206644c2aab0b2c5f0c
Fatér Zsolt <zsolt.fater@idomsoft.hu>
2025-07-22 15:57:40 +0200

    websocket server

--- employees-backend/src/main/java/employees/EmployeesApplication.java
+++ employees-backend/src/main/java/employees/EmployeesApplication.java
@@ -2,8 +2,10 @@ package employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @SpringBootApplication
+@EnableWebSocketMessageBroker
 public class EmployeesApplication {
 
 	public static void main(String[] args) {
