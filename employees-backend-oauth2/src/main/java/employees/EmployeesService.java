fb503c7c8c598c4f51fde127832bddd72b75b755
Istvan Viczian <viczian.istvan@gmail.com>
2025-07-23 12:38:57 +0200

    OAUTH 2.0

---
+++ employees-backend-oauth2/src/main/java/employees/EmployeesService.java
@@ -0,0 +1,49 @@
+package employees;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeesService {
+
+    private final EmployeesRepository repository;
+
+    public List<EmployeeDto> listEmployees() {
+        return repository.findAllResources();
+    }
+
+    public EmployeeDto findEmployeeById(long id) {
+        return toDto(repository.findById(id).orElseThrow(notFountException(id)));
+    }
+
+    public EmployeeDto createEmployee(EmployeeDto command) {
+        Employee employee = new Employee(command.name());
+        repository.save(employee);
+        return toDto(employee);
+    }
+
+    @Transactional
+    public EmployeeDto updateEmployee(long id, EmployeeDto command) {
+        Employee employee = repository.findById(id).orElseThrow(notFountException(id));
+        employee.setName(command.name());
+        return toDto(employee);
+    }
+
+    public void deleteEmployee(long id) {
+        repository.deleteById(id);
+    }
+
+    private EmployeeDto toDto(Employee employee) {
+        return new EmployeeDto(employee.getId(), employee.getName());
+    }
+
+    private Supplier<EmployeeNotFoundException> notFountException(long id) {
+        return () -> new EmployeeNotFoundException("Employee not found with id: %d".formatted(id));
+    }
+
+}
