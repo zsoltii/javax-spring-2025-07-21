5241f68ba6e39033d2d33381a1820532d5166282
Fatér Zsolt <zsolt.fater@idomsoft.hu>
2025-07-23 13:04:40 +0200

    add frontend

--- employees-frontend/src/main/java/employees/EmployeesClient.java
+++ employees-frontend/src/main/java/employees/EmployeesClient.java
@@ -1,7 +1,6 @@
 package employees;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.GetExchange;
