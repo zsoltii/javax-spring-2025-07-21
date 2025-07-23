5989fd49553cbda94ce3f0bc356a3135a1bb16c5
Istvan Viczian <viczian.istvan@gmail.com>
2025-07-23 14:44:51 +0200

    Backend OAuth 2.0 config

---
+++ employees-backend-oauth2/src/main/java/employees/SecurityConfig.java
@@ -0,0 +1,30 @@
+package employees;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                registry -> registry
+                        .requestMatchers(HttpMethod.POST, "/api/employees")
+                        .authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/employees/**")
+                        .authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**")
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll()
+        )
+                .oauth2ResourceServer(conf
+                        -> conf.jwt(Customizer.withDefaults()));
+                return http.build();
+    }
+}
