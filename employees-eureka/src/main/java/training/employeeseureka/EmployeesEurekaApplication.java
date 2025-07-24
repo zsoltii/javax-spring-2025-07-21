dc5ca670369c19eefa2e9439f0912be1a2343400
Istvan Viczian <viczian.istvan@gmail.com>
2025-07-24 16:46:42 +0200

    Eureka kiindulási állapot

---
+++ employees-eureka/src/main/java/training/employeeseureka/EmployeesEurekaApplication.java
@@ -0,0 +1,13 @@
+package training.employeeseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesEurekaApplication.class, args);
+    }
+
+}
---
+++ employees-eureka/src/test/java/training/employeeseureka/EmployeesEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package training.employeeseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
