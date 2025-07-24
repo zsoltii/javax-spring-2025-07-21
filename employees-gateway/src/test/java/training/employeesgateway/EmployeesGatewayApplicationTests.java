8ffca8dbf8b14c38d01d70f0738c700ed0578f84
Istvan Viczian <viczian.istvan@gmail.com>
2025-07-24 16:29:44 +0200

    Gateway kiindulás

---
+++ employees-gateway/src/test/java/training/employeesgateway/EmployeesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package training.employeesgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
