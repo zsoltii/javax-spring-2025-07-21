e1e00ea1bd70fe1581686164872a6b4e7876fffb
Istvan Viczian <viczian.istvan@gmail.com>
2025-07-24 09:47:00 +0200

    GraphQL előkészítés

---
+++ employees-backend-graphql/src/main/java/empapp/AddressRepository.java
@@ -0,0 +1,11 @@
+package empapp;
+
+import empapp.entity.Address;
+import empapp.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
